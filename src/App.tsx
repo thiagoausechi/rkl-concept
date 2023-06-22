1587bebc1aeb106f94411e14e6c4bc1551e3561a
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-06-22 08:36:16 -0300

    feat: :tada: Started the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 style={{ marginBlockEnd: 0 }}>Random Key Link</h1>
+      <p style={{ marginBlockStart: 0 }} className="tip">
+        concept
+      </p>
+    </>
+  );
+}
+
+export default App;
